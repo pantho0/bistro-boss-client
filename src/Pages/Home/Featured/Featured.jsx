6653f43070f431a0db7b22f71d1288e847fe139b
pantho0 <panthomashreky6@gmail.com>
2023-11-10 21:56:37 +0600

    dynamic  testimonial section added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -3,9 +3,9 @@ import featuredImg from '../../../assets/home/featured.jpg'
 import './Featured.css'
 const Featured = () => {
     return (
-        <div className="featured-image text-white pt-8 my-20">
+        <div className="featured-image bg-fixed  text-white pt-8 my-20">
             <SectionTitle subHeading='Check it out' heading='Featured Item'></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center pb-20 pt-12 px-36 ">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
