0fdccc08f0018098f4809b3aad6a7d9264f32f88
pantho0 <panthomashreky6@gmail.com>
2023-11-10 17:08:09 +0600

    banner slider added with images and positions

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
    </>
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black max-w-screen-xl mx-auto text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
