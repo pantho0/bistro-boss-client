d5f497d13a831b35180a356eef79f66b6f5fcdef
pantho0 <panthomashreky6@gmail.com>
2023-11-11 01:10:59 +0600

    react helmet added for tab title

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,11 @@
-
+import {Helmet} from 'react-helmet-async';
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
             <h2>Menu</h2>
         </div>
     );
