2ad502a4db23edcd544cd47d61362fc5785220c3
pantho0 <panthomashreky6@gmail.com>
2023-11-12 13:47:52 +0600

    react tabs implemented for dynamically load foods by their category

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     const navLinks = <>
         <li><Link to={'/'}>Home</Link></li>
         <li><Link to={'/menu'}>Our Menu</Link></li>
+        <li><Link to={'/order'}>Orders</Link></li>
         </>
         return (
         <>
