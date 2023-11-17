da243c75a52701a96283f8a1094e70c6f391995e
pantho0 <panthomashreky6@gmail.com>
2023-11-17 20:01:49 +0600

    custom middleware added for recive client side access token

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
+import useAxiosPublic from "../Components/Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null)
 
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const axiosPublic = useAxiosPublic()
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
     const provider = new GoogleAuthProvider()
@@ -37,6 +39,18 @@ const AuthProvider = ({children}) => {
        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser)
             console.log('logged User : ', currentUser);
+            const userInfo = {email : currentUser?.email}
+            if(currentUser){    
+                axiosPublic.post('/jwt', userInfo)
+                .then(res =>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token)
+                    }
+                })
+            }else{
+                // TODO : Remove access token
+                localStorage.removeItem('access-token')     
+            }
             setLoading(false)
         });
         return ()=>{
