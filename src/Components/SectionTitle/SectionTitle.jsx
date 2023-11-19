3e762412211a00cec6dbe4687d7c8d7fa72f09dc
pantho0 <panthomashreky6@gmail.com>
2023-11-19 18:00:53 +0600

    add item page created with react hook form

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -4,7 +4,7 @@ const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className="mx-auto text-center mb-6 mt-10 md:w-4/12">
             <p className="text-yellow-600 italic mb-4">--- {subHeading} ---</p>
-            <h3 className="text-3xl border-y-4 py-4">{heading}</h3>
+            <h3 className="text-3xl border-y-4 py-4 uppercase">{heading}</h3>
         </div>
     );
 };
--- src/Dashboard/Add Items/AddItems.jsx
+++ src/Dashboard/Add Items/AddItems.jsx
@@ -1,12 +1,84 @@
-import React from 'react';
-import SectionTitle from '../../Components/SectionTitle/SectionTitle';
+import React from "react";
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
-    return (
-        <div>
-            <SectionTitle heading={'add an item'} subHeading={"What's new"}></SectionTitle>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div>
+      <SectionTitle
+        heading={"add an item"}
+        subHeading={"What's new"}
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text">Recipie Name?</span>
+            </label>
+            <input
+              type="text"
+              {...register("name")}
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="flex gap-6">
+            {/* Category */}
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Category</span>
+              </label>
+              <select
+                {...register("category")}
+                className="select select-bordered w-full"
+              >
+                <option disabled >
+                  Select Category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </div>
+            {/* Price */}
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="nuber"
+                {...register("price")}
+                placeholder="Price"
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+          {/* Recipe Details */}
+          <div className="form-control my-6">
+            <label className="label">
+              <span className="label-text">Recipe Details</span>
+            </label>
+            <textarea
+              {...register("recipe")}
+              className="textarea textarea-bordered h-24"
+              placeholder="Bio"
+            ></textarea>
+          </div>
+          <div className="form-control w-full my-6">
+          <input {...register("image")} type="file" className="file-input w-full max-w-xs" />
+          </div>
+          <button className="btn">
+            Add Item <FaUtensils></FaUtensils>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
