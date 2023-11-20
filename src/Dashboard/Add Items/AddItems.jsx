88c3d9213daeb61f8d561a4be3d64b938caa5ac6
pantho0 <panthomashreky6@gmail.com>
2023-11-20 12:57:05 +0600

    Add food item added with secure posting method

--- src/Dashboard/Add Items/AddItems.jsx
+++ src/Dashboard/Add Items/AddItems.jsx
@@ -2,10 +2,49 @@ import React from "react";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
 import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../Components/Hooks/useAxiosPublic";
+import useAxiosSecure from "../../Components/Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const onSubmit = async(data) => {
+    console.log(data);
+    const imageFile = {image : data.image[0]}
+    // img upload to img bb and then get url.
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers:{
+        'content-type':"multipart/form-data"
+      }
+    })
+    if(res.data.success){
+      // now send the data to the server with the img url
+      const menuItem = {
+        name : data.name,
+        category : data.category,
+        price : parseFloat(data.price),
+        recipe : data.recipe,
+        image : res.data.data.display_url
+      }
+      const menuRes = await axiosSecure.post('/menu', menuItem)
+      console.log(menuRes.data);
+      if(menuRes.data.insertedId){
+        reset()
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is Added`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    }
+    console.log('with image url',res.data);
+  };
   return (
     <div>
       <SectionTitle
@@ -32,10 +71,11 @@ const AddItems = () => {
                 <span className="label-text">Category</span>
               </label>
               <select
+                defaultValue={"default"}
                 {...register("category")}
                 className="select select-bordered w-full"
               >
-                <option disabled >
+                <option disabled value="default">
                   Select Category
                 </option>
                 <option value="salad">Salad</option>
