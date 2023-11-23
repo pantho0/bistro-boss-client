60ed4ea13175bb5048a0a42504479d254000ccbd
pantho0 <panthomashreky6@gmail.com>
2023-11-23 17:13:49 +0600

    conditional admin & user dashboard navlink added to navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { FaShoppingCart } from "react-icons/fa";
 import useCart from "../../../Components/Hooks/useCart";
+import useAdmin from "../../../Components/Hooks/useAdmin";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
   const [cart] = useCart();
+  const [isAdmin] = useAdmin();
   const handleLogout = () => {
     logOut();
   };
@@ -21,9 +23,16 @@ const Navbar = () => {
       <li>
         <Link to={"/order/salad"}>Orders</Link>
       </li>
-      <li>
-        <Link to={"/secret"}>secret</Link>
+      {
+        user && isAdmin && <li>
+        <Link to={"/dashboard/adminHome"}>Dashboard</Link>
       </li>
+      }
+      {
+        user && !isAdmin && <li>
+        <Link to={"/dashboard/userHome"}>Dashboard</Link>
+      </li>
+      }
       <li>
         <Link to={"/dashboard/cart"}>
           <button className="btn">
